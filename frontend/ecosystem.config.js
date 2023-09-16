e48d46cc56ce35f0c6c8f3d17dc29ef323da6e26
Kitybot <nick34goalie@yandex.ru>
2023-09-16 18:17:02 +0300

    commit

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,16 +1,10 @@
-const dotenvConfig = require('dotenv').config;
-
-dotenvConfig({ path: '.env.deploy' });
+require('dotenv').config({path: "./.env.deploy"});
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_PATH, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: 'mesto-frontend',
-    script: './src/app.ts',
-  }],
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -18,7 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd /home/deploy/mesto-frontend && npm i && export NODE_OPTIONS=--openssl-legacy-provider && npm run build',
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
     },
   },
 };
