367f77c8ae71aeb814779ae96cc2c83339d013bb
Kitybot <nick34goalie@yandex.ru>
2023-09-16 18:20:07 +0300

    commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
 }
 }
 // Замените на адрес вашего бэкенда
-const api = new Api('http://localhost:3000');
+const api = new Api('http://api.mestokirbob.students.nomoredomainsicu.ru');
 
 export default api;
